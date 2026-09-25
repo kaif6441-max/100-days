675b66ea0745eff4b0e41719acba80b30a5b3939
kaif6441-max <kaif6441@gmail.com>
2026-09-26 01:07:41 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (16.05%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int fun(int n,int[] dp){
+        if(n==0||n==1) return n;
+        if(n==2) return 1;
+        if(dp[n]!=-1) return dp[n];
+
+        return dp[n] = fun(n-1,dp)+fun(n-2,dp)+fun(n-3,dp);
+    }
+    public int tribonacci(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        
+        return fun(n,dp);
+        
+    }
+}
